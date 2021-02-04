db9cf11243c4309050f61284c4aaab2b6139ea1e
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-02-04 12:11:34 +0300

    entities

---
+++ src/ApplicationCore/Entities/Buyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Buyer : BaseEntity
+    {
+        public string IdentityGuid { get; set; }
+    }
+}
