01ef1cbd469c348110e365e1993e3db707e0cfcc
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-02-11 13:17:27 +0300

    IBasketService

---
+++ src/Web/Controllers/BasketController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult AddToBasket(ProductViewModel productDetails)
+        {
+            // todo: sepet yoksa oluştur
+
+            // todo: oluşan sepeti ilgili ürünü ekle
+
+            return Json(new { BasketItemCount = 0 });
+        }
+    }
+}
